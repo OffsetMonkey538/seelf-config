ebef4da64e475dc40ae7737f3054d0406ca26963
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-07-17 16:38:57 +0300

    pls

--- example-site/build.sh
+++ example-site/build.sh
@@ -2,3 +2,4 @@
 
 pnpm run build
 tar -cvf archive.tar.gz dist docker-compose.yml
+curl -i -X POST -H \"Authorization: Bearer $SEELF_TOKEN\" -F environment=$SEELF_ENVIRONMENT -F archive=@archive.tar.gz $SEELF_APPLICATION_URL
