46c5eac2b309392977a33cac1daeb05804225c6a
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-07-17 18:33:41 +0300

    ok come on it'll work now, right?

--- example-site/build.sh
+++ example-site/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 pnpm run build
-tar -cvf archive.tar.gz dist docker-compose.yml
+tar -czvf archive.tar.gz dist docker-compose.yml
 curl -i -X POST -H "Authorization: Bearer $SEELF_TOKEN" -F environment=$SEELF_ENVIRONMENT -F archive=@archive.tar.gz $SEELF_APPLICATION_URL
